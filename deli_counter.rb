12fcd3b732a9937423ac05e9f7dc3b9584c62a4e
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-06-08 19:05:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line (katz_deli)
     puts "The line is currently empty."
   else 
     current_line = "The line is currently:"
-    katz_deli.each.index(1) do |name, i|
+    katz_deli.each.with_index(1) do |name, i|
       current_line << " #{i}. #{name}"
     end
     puts current_line
