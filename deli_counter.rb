fd5e2e24c2b90c5fde0ba4ce13f485990a97785d
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-06-08 18:53:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,5 +6,6 @@ def line (katz_deli)
   else 
     current_line = "The line is currently: "
     katz_deli.each.index(1) do |name, i|
-      current_line << #{name, i
+      current_line << #{i}. #{person}
+    
       
